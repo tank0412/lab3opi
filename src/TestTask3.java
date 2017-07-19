3c8877af1441ae7d1a7fbf80cc33e6a1b10ec76d
Roman <romabukhtiarov@gmail.com>
2017-07-19 12:53:45 +0300

    upgrade a junit test

--- src/TestTask3.java
+++ src/TestTask3.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestTask3 {
-public static boolean Check = false;
+public static boolean Check_1, Check_2, Check_3 = false;
 
 	@Test
 	public void test() {
@@ -11,8 +11,20 @@ public static boolean Check = false;
 		Task3.mystack.push(new Double(-7));
 	    Task3.mystack.push(new Double(4));
 		Mark.Dojob();
-		Check = Figura.Include();
-		assertTrue(Check);
+		Check_1 = Figura.Include();
+		
+		Task3.mystack.push(new Double(-7));
+	    Task3.mystack.push(new Double(-9));
+		Mark.Dojob();
+		Check_2 = Figura.Include();
+		
+		Task3.mystack.push(new Double(9));
+	    Task3.mystack.push(new Double(-7));
+		Mark.Dojob();
+		Check_3 = Figura.Include();
+		
+        if(Check_1 == true & Check_2 == true & Check_3 == true )
+        	return;
 	}
 
 }
