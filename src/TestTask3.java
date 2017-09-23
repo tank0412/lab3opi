0cdb256996684d7a75df7ea285a8b8c6766f2b9b
Roman <romabukhtiarov@gmail.com>
2017-09-23 12:53:57 +0300

    update junit test

--- src/TestTask3.java
+++ src/TestTask3.java
@@ -18,13 +18,15 @@ public static boolean Check_1, Check_2, Check_3 = false;
 		Mark.Dojob();
 		Check_2 = Figura.Include();
 		
-		Task3.mystack.push(new Double(9));
-	    Task3.mystack.push(new Double(-7));
+		Task3.mystack.push(new Double(1000));
+	    Task3.mystack.push(new Double(1000));
 		Mark.Dojob();
 		Check_3 = Figura.Include();
 		
-        if(Check_1 == true & Check_2 == true & Check_3 == true )
+        if(Check_1 == true & Check_2 == true & Check_3 == false )
         	return;
+        else
+        	fail();
 	}
 
 }
